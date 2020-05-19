f103539cf27bca039d37f5ae5f46ebc5e99fc817
Nate <hunter.j.nate@gmail.com>
2020-05-19 19:26:16 -0400

    accepts a directory of A&E metadata and converts to a single CSV file

--- PROJ_QUESTIONS.md
+++ PROJ_QUESTIONS.md
@@ -1,3 +1,7 @@
 # QUESTIONS:
-1.) Should I be using a virtual environment?
-2.) Should I use `openpyxl` or `csv` or...?
+* Should I be using a virtual environment?
+* How to read XML files with a declaration?
+* Should I use `openpyxl` or `csv` or...?
+* What is the best way to iterate over files in directory, eg. `os.listdir()`, `os.scandir()`, or `os.walk()`?
+* How should I handle errors?
+* How to implement tests?
--- xml_to_csv.py
+++ xml_to_csv.py
@@ -1,10 +1,11 @@
 import lxml.etree as etree
 import csv
+import os
 
 
-def create_data_dict(xml_to_parse):
-    '''DATA PROVIDED BY A & E'''
-    with open(xml_to_parse) as f:
+def create_ae_data_dict(xml_to_parse):  # DATA PROVIDED BY A&E
+
+    with open(xml_to_parse, encoding='utf-8') as f:
         open_xml = f.read()
 
     root = etree.fromstring(open_xml)
@@ -31,21 +32,47 @@ def create_data_dict(xml_to_parse):
     return data_dict
 
 
-def write_to_csv(filename, data):
+# TAKES A DIRECTORY OF XML METADATA AND OUTPUTS A LIST WHICH IS USED BY
+# `write_to_csv()`.
+def process_directory(directory):
+    list_data = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".xml"):
+            file = os.path.join(directory, filename)
+            ae_dict = create_ae_data_dict(file)
+            list_data.append(ae_dict)
+    return list_data
+
+
+def extract_columns(data):  # EXTRACTS COLUMNS TO USE IN `DictWriter()`
     columns = []
 
-    for k in data:
-        columns.append(k)
+    column_headers = data[0]
 
-    with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=columns)
+    for key in column_headers:
+        columns.append(key)
+
+    return columns
+
+
+def write_to_csv(filename, data):
+    columns = extract_columns(data)
+
+    with open(filename, 'w', newline='') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=columns)
         writer.writeheader()
-        writer.writerow(data)
+        for row in data:
+            writer.writerow(row)
 
 
-xml_to_parse = '.\\xml_files\\test_xml_episode1.xml'
+xml_to_parse = '.\\xml_files\\S10 E9 - AETN-222628PAR_THC_AXMN_S010-CONTENT_HD-SnappinTrees-v202004242259.xml'
 
+''' RESOLVE THE ERROR BELOW TO READ XML FILES WITH A DECLARATION:
+        ValueError: Unicode strings with encoding declaration are not supported. Please use bytes input or XML fragments without declaration.
+'''
+
+directory = '.\\xml_files'
 
 if __name__ == '__main__':
-    data = create_data_dict(xml_to_parse)
-    write_to_csv('csv_output.csv', data)
+    ae_data = process_directory(directory)
+    write_to_csv('csv_output.csv', ae_data)
